553022f1e09f750155c94d85fa4b14d82717c871
sergey-untilov <s.untilov@gmail.com>
2020-04-22 04:57:04 +0300

    Fixed ID in the employees list for the timesheet reduce working time.

--- assets/ispro/Скорочення робочого дня-тижня - працівники (hr_timeSheetChangeEmp).sql
+++ assets/ispro/Скорочення робочого дня-тижня - працівники (hr_timeSheetChangeEmp).sql
@@ -1,5 +1,6 @@
 -- Скорочення робочого дня-тижня - працівники (hr_timeSheetChangeEmp)
 declare @dateFrom date = dateadd(month, -3,(select cast(cast((year(getdate()) - 1) * 10000 + 101 as varchar(10)) as date)))
+declare @shiftID bigint = 100000
 /*BEGIN-OF-HEAD*/
 select
 	'ID' ID
@@ -8,8 +9,8 @@ select
 union all	
 /*END-OF-HEAD*/
 select
-	cast(bookmark as varchar) ID	
-	,cast(bookmark as varchar) timeSheetChangeID	
+	cast(bookmark + @shiftID as varchar) ID	
+	,cast(bookmark + @shiftID as varchar) timeSheetChangeID	
 	,cast(kpu_rcd as varchar) employeeNumberID
 from KpuIgrB
 where kpuigr_datEnd <= '1876-12-31' or kpuigr_datEnd >= @dateFrom
