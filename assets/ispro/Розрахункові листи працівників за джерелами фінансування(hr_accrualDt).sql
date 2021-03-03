e75cc95c11d1ff0c537779878881cdcc3634e6c1
sergey-untilov <s.untilov@gmail.com>
2021-03-03 12:36:10 +0200

    Fix hr_accrualDt.

--- assets/ispro/Розрахункові листи працівників за джерелами фінансування(hr_accrualDt).sql
+++ assets/ispro/Розрахункові листи працівників за джерелами фінансування(hr_accrualDt).sql
@@ -1,4 +1,4 @@
--- ����������� ����� ���������� (hr_accrual)
+--  (hr_accrual)
 declare @sysste_rcd bigint = (select max(sysste_rcd) from sysste where sysste_cd = /*SYSSTE_CD*/)
 declare @sprpdr_cd nvarchar(20) = /*SPRPDR_CD*/
 declare @dateFrom date = dateadd(month, -3,(select cast(cast((year(getdate()) - 1) * 10000 + 101 as varchar(10)) as date)))
@@ -12,12 +12,11 @@ declare @currentPeriod date = (
 select 
 	cast(z1.bookmark as varchar) ID	
 	,cast(r1.bookmark as varchar) accrualID	
-	,cast(CONVERT(DECIMAL(19, 0), z1.KpuRlSPZ_Sm) / 100 as varchar) paySum	
+	,cast(CONVERT(DECIMAL(19, 0), coalesce(z1.KpuRlSPZ_Sm, r1.kpurl_Sm)) / 100 as varchar) paySum	
 	,cast(r1.kpurl_SF as varchar) dictFundSourceID	
 	,case when pdr1.KpuPrkz_PdRcd > 0 then cast(pdr1.KpuPrkz_PdRcd as varchar) else '' end departmentID	
-	,case when z1.KpuRl_CdSch > 0 then cast(z1.KpuRl_CdSch as varchar) else '' end accountID
+	,case when z1.KpuRl_CdSch is not null and z1.KpuRl_CdSch > 0 then cast(z1.KpuRl_CdSch as varchar) else '' end accountID
 from kpurlo1 r1
-inner join kpurlospz z1 on z1.kpu_tn = r1.kpu_tn and z1.KpuRl_DatRp = r1.KpuRl_DatRp and z1.KpuRl_Rcd = r1.KpuRl_Rcd
 inner join KPUX x1 on x1.Kpu_Tn = r1.Kpu_Tn
 inner join KPUC1 c1 on c1.Kpu_Rcd = x1.kpu_rcd
 
@@ -32,6 +31,7 @@ inner join kpuprk1 pdr1 on pdr1.kpu_rcd = c1.kpu_rcd and pdr1.bookmark = (
 ) and (@sprpdr_cd = '' or @sprpdr_cd = left(pdr1.kpuprkz_pd, len(@sprpdr_cd)))
 
 inner join PAYVO1 v1 on v1.Vo_Cd = r1.kpurl_cdvo
+left join kpurlospz z1 on z1.kpu_tn = r1.kpu_tn and z1.KpuRl_DatRp = r1.KpuRl_DatRp and z1.KpuRl_Rcd = r1.KpuRl_Rcd
 where r1.KpuRl_CdVo <> 0
 	and r1.KpuRl_DatUp >= @dateFrom
 	and r1.KpuRl_DatUp < @currentPeriod
