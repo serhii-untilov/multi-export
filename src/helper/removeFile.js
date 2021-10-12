e7dad8f36695f2f89cbfb72d116498fb8b4e1897
Sergey Untilov <s.untilov@gmail.com>
2021-10-12 14:32:58 +0300

    Asynchronous delete file

--- src/helper/removeFile.js
+++ src/helper/removeFile.js
@@ -2,15 +2,21 @@
 
 const fs = require('fs')
 
-async function removeFile (fileName) {
-    fs.access(fileName, fs.F_OK, (err) => {
-        if (err) { return }
-        fs.unlink(fileName, (err) => {
-            if (err) {
-                console.log(err)
-            }
-        })
-    })
+// async function removeFile (fileName) {
+//     fs.access(fileName, fs.F_OK, (err) => {
+//         if (err) { return }
+//         fs.unlink(fileName, (err) => {
+//             if (err) {
+//                 console.log(err)
+//             }
+//         })
+//     })
+// }
+
+function removeFileSync (fileName) {
+    if (fs.existsSync(fileName)) {
+        fs.unlinkSync(fileName)
+    }
 }
 
-module.exports = removeFile
+module.exports = removeFileSync
