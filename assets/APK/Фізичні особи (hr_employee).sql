896fe0030b8e285b4f76874831c3e82a23c2e0e2
Sergey Untilov <s.untilov@gmail.com>
2021-10-13 16:18:59 +0300

    APK started

---
+++ assets/APK/Фізичні особи (hr_employee).sql
@@ -0,0 +1,13 @@
+--  Фізичні особи
+select 
+	base.uuid_bigint(id::text) ID,
+	ident_code taxCode,
+	name_last lastName,
+	name_first firstName,
+	name_middle middleName,
+	case when sex is true then 'M' else 'W' end,
+	birth_date birthDate,
+	concat(name_last, ' ', substring(name_first from 1 for 1), '. ', substring(name_middle from 1 for 1), '.') shortFIO,
+	concat(name_last, ' ', name_first, ' ', name_middle) fullFIO,
+	'NEW' state
+from base.dict_person;
