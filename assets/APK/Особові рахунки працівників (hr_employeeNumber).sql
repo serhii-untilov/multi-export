8ac49118e8f147bc77d8daa96f4637ae4004815e
sergey-untilov <s.untilov@gmail.com>
2021-10-22 15:55:24 +0300

    APK

---
+++ assets/APK/Особові рахунки працівників (hr_employeeNumber).sql
@@ -0,0 +1,21 @@
+select base.uuid_bigint(e1.id::text) "ID",
+	base.uuid_bigint(e1.id::text) "employeeID",
+	h1.employe_no "tabNum",
+	e1.ident_code "taxCode",
+	e1.name_last "lastName",
+	e1.name_first "firstName",
+	e1.name_middle "middleName",
+	cast(cast(h1.from_date as date) as varchar) "dateFrom",
+	'' "dateTo" --cast(cast(d1.from_date as date) as varchar) "dateTo"
+	,'' "payOutID"
+	,'' "personalAccount"
+	,cast(cast(h1.from_date as date) as varchar) "appointmentDate"
+	,cast(cast(h1.issued_at as date) as varchar) "appointmentOrderDate"
+	,h1.no "appointmentOrderNumber"
+	,concat(e1.name_last, ' ', e1.name_first, ' ', e1.name_middle, ' [', h1.employe_no, ']') "description"
+from staff.employe e1
+inner join staff.doc_hiring h1 on h1.employe_id = e1.id
+left join staff.info_staff i1 on i1.employe_owner_id = e1.id
+inner join staff.work_place wp on wp.keeper_id = e1.id
+left join staff.doc_dismissal d1 on d1.id = wp.doc_dismissal_id 
+where d1.from_date is null
--- assets/APK/Фізичні особи (hr_employee).sql
+++ assets/APK/Фізичні особи (hr_employee).sql
@@ -1,13 +1,13 @@
 --  Фізичні особи
 select 
-	base.uuid_bigint(id::text) ID,
-	ident_code taxCode,
-	name_last lastName,
-	name_first firstName,
-	name_middle middleName,
-	case when sex is true then 'M' else 'W' end,
-	birth_date birthDate,
-	concat(name_last, ' ', substring(name_first from 1 for 1), '. ', substring(name_middle from 1 for 1), '.') shortFIO,
-	concat(name_last, ' ', name_first, ' ', name_middle) fullFIO,
+	base.uuid_bigint(id::text) "ID",
+	ident_code "taxCode",
+	name_last "lastName",
+	name_first "firstName",
+	name_middle "middleName",
+	case when sex is true then 'M' else 'W' end "sexType",
+	cast(cast(birth_date as date) as varchar) "birthDate",
+	concat(name_last, ' ', substring(name_first from 1 for 1), '. ', substring(name_middle from 1 for 1), '.') "shortFIO",
+	concat(name_last, ' ', name_first, ' ', name_middle) "fullFIO",
 	'NEW' state
-from base.dict_person;
+from staff.employe; --base.dict_person;
