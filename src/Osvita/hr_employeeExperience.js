cf7bebdde5a350493fb53c9051896f495b9dbfba
Sergey Untilov <s.untilov@gmail.com>
2021-11-22 11:07:43 +0200

    v1.4.05

--- src/Osvita/hr_employeeExperience.js
+++ src/Osvita/hr_employeeExperience.js
@@ -18,7 +18,7 @@ function setRecord (record, recordNumber) {
     this.entity[0].employeeNumberID = ID
     const experience = this.dictionary.getExperienceByName(record.FAM, record.IM, record.OT)
     if (experience) {
-        const baseDate = new Date('2021-09-01')
+        const baseDate = new Date('2021-10-01')
         baseDate.setFullYear(baseDate.getFullYear() - experience.years)
         baseDate.setMonth(baseDate.getMonth() - experience.months)
         baseDate.setDate(baseDate.getDate() - experience.days)
