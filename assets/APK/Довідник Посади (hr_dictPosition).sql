571babb3181b11062df6a7edc6a5e9ac3e198e59
sergey-untilov <s.untilov@gmail.com>
2022-05-11 11:07:45 +0300

    v1.4.13

--- assets/APK/Довідник Посади (hr_dictPosition).sql
+++ assets/APK/Довідник Посади (hr_dictPosition).sql
@@ -9,7 +9,8 @@ select base.uuid_bigint(p1.id::text) "ID",
 	NULLIF(regexp_replace(p1.work_period_time::text, '\D','','g'), '') "workScheduleID",
 	nullif(c1.alias, '') "dictStaffCatID",
 	left(case when p1.description is not null then concat(p1.name, ', ', p1.description) else p1.name end, 256) "description",
-	left(case when p1.code is not null then concat(p1.name, '[', p1.code, ']') else p1.name end, 256) "caption"
+	left(case when p1.code is not null then concat(p1.name, '[', p1.code, ']') else p1.name end, 256) "caption", 
+	p1.work_period_time "weekHours"
 from staff.dict_work_post p1
 left join staff.enum_work_post_category c1 on c1.id = p1.work_post_category_id 
 where p1.deleted_at is null
