25742dacb5229e0d564498df30af7ddb6fd3d816
Sergey Untilov <s.untilov@gmail.com>
2023-01-20 19:40:17 +0200

    Improve Bossk

--- assets/bossk/Розрахункові листи (hr_accrual).sql
+++ assets/bossk/Розрахункові листи (hr_accrual).sql
@@ -1,4 +1,5 @@
 declare @orgID bigint = (case when /*OKPO*/ = '' then null else coalesce((select ID from HR_FIRM where OKPO = /*OKPO*/), -1) end)
+declare @minDate date = DATEADD(YEAR, DATEDIFF(year, 0, DATEADD(month, -18, DATEADD(month, DATEDIFF(month, 0, getDate()), 0))), 0)
 select -- c1.Full_Name, p1.Num_Tab, d1.Name_Pay, 
 	ID = Auto_Lic
 	,periodCalc = cast(cast(DATEADD(month, DATEDIFF(month, 0, dbo.zrp_fn_CMONTHTOD(l1.days, l1.cmonth)), 0) as date) as varchar)
@@ -26,4 +27,9 @@ inner join people p1 on p1.pId = l1.pid
 inner join Card c1 on c1.Auto_Card = p1.Auto_Card
 left join tabel t1 on t1.Auto_Tabel = l1.tabel_id
 where (@orgID is null or @orgID = p1.id_Firm)
+and ( 
+    cast(DATEADD(month, DATEDIFF(month, 0, dbo.zrp_fn_CMONTHTOD(l1.days, l1.cmonth)), 0) as date) >= @minDate 
+    OR
+    cast(DATEFROMPARTS(l1.tYear, l1.tMonth, 1) as date) >= @minDate
+)
 order by p1.Num_Tab, dbo.zrp_fn_CMONTHTOD(l1.days, l1.cmonth), d1.Code_Pay
