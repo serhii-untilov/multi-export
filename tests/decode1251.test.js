b91c294f23104395bfea1ee44391c83d9845f5d2
Sergey Untilov <s.untilov@gmail.com>
2023-01-25 14:52:50 +0200

    Decode test

--- tests/decode1251.test.js
+++ tests/decode1251.test.js
@@ -1,10 +1,11 @@
 const iconv = require('iconv-lite')
 
 test('decode 1251 latin and cyrillic', () => {
-    const utf8 = 'abc123абв'
+    const src = 'abc123абв'
+    const utf8 = iconv.encode(src, 'utf8')
     const cp1251 = iconv.encode(utf8, 'win1251')
     const result = iconv.decode(cp1251, 'win1251')
     console.log(utf8, cp1251, result)
 
-    expect(result).toBe(utf8)
+    expect(result).toBe(src)
 })
