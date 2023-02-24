ddde37de9d8ceff7b5b0eabe69916e4ddf62c69a
nats6331 <nats6331@gmail.com>
2023-02-24 15:23:52 +0200

    HR_BOSS_ARTEMENKO24_Leave

--- assets/bossk/Персонал. Відпустки архів (hr_employeeVacation).sql
+++ assets/bossk/Персонал. Відпустки архів (hr_employeeVacation).sql
@@ -15,6 +15,6 @@ from pr_Leave
 join people ON people.pid = pr_Leave.pId
 join typ_Leave on pr_Leave.Code_Leave = typ_Leave.Code_Leave
 left join PR_ORDERS on pr_Leave.Refer_Num = PR_ORDERS.Refer_Num
-where people.out_date = '1900-01-01'
+where ( (people.out_date = '1900-01-01') or ( people.out_date>='2022-01-01'))
     and typ_Leave.Code_Operat <> 3
     and (@orgID is null or @orgID = people.id_Firm)
