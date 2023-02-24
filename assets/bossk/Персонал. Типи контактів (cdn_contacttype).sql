23978f6ecc635879fb9c87ac2ff8d254da40bd91
nats6331 <nats6331@gmail.com>
2023-02-24 15:14:56 +0200

    HR_BOSS_ARTEMENKO24_03

--- assets/bossk/Персонал. Типи контактів (cdn_contacttype).sql
+++ assets/bossk/Персонал. Типи контактів (cdn_contacttype).sql
@@ -1,10 +1,13 @@
+-- типи контактів
 select 
     phone_type_code as ID,
     phone_type_code as code,
     phone_type_name as name
 from phone_types
+
 union
+
 select 
     999 as ID,
-    т999 as code,
-    'Внуренний' as name
+    999 as code,
+    'Внурішній' as name
