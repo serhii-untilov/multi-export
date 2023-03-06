0d344657e6188eadcc23925ac57014e3642e506c
Sergey Untilov <s.untilov@gmail.com>
2023-03-06 17:30:02 +0200

    Changed Postgres tests

--- tests/postgres.test.js
+++ tests/postgres.test.js
@@ -8,25 +8,27 @@ const POOL_SIZE = 4
 const CONNECTION_TIMEOUT = 20 * 60 * 1000 // 20 minutes
 const REQUEST_TIMEOUT = 20 * 60 * 1000 // 20 minutes
 
-test.skip('postgres', async () => {
-    const testString = 'abc123абв'
-    dbConfig.connectionTimeoutMillis = CONNECTION_TIMEOUT
-    dbConfig.idleTimeoutMillis = REQUEST_TIMEOUT
-    dbConfig.max = POOL_SIZE
-    const pool = new Pool(dbConfig)
-    pool.on('error', (err) => {
-        console.log(err)
+describe.skip('Postgres tests', () => {
+    test('postgres', async () => {
+        const testString = 'abc123абв'
+        dbConfig.connectionTimeoutMillis = CONNECTION_TIMEOUT
+        dbConfig.idleTimeoutMillis = REQUEST_TIMEOUT
+        dbConfig.max = POOL_SIZE
+        const pool = new Pool(dbConfig)
+        pool.on('error', (err) => {
+            console.log(err)
+        })
+        const client = await pool.connect()
+        const query1 = `select '${testString}' "testString";`
+        let query2 = await (async () => readQueryFromFile('./Test.sql'))()
+        query2 = query2.replace(/\n/g, ' ').replace(/\r/g, ' ').replace(/\s{2,}/gm, ' ').trim()
+        query2 = query2.replace(/\r/g, ' ').trim()
+        expect(query2).toBe(query1)
+        const res = await client.query(query2)
+        client.release(true)
+        const response = res.rows.length ? res.rows[0].testString : null
+        expect(response).toBe(testString)
     })
-    const client = await pool.connect()
-    const query1 = `select '${testString}' "testString";`
-    let query2 = await (async () => readQueryFromFile('./Test.sql'))()
-    query2 = query2.replace(/\n/g, ' ').replace(/\r/g, ' ').replace(/\s{2,}/gm, ' ').trim()
-    query2 = query2.replace(/\r/g, ' ').trim()
-    expect(query2).toBe(query1)
-    const res = await client.query(query2)
-    client.release(true)
-    const response = res.rows.length ? res.rows[0].testString : null
-    expect(response).toBe(testString)
 })
 
 function readQueryFromFile (fileName) {
